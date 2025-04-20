0353ce7eb5349bb999853c18800c919cea2b04a0
William Mon <123139857+williammyo@users.noreply.github.com>
2025-04-19 22:18:01 -0700

    Create discord_logger.py

---
+++ discord_logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+import aiohttp
+import asyncio
+from dotenv import load_dotenv
+
+load_dotenv()
+WEBHOOK_URL = os.getenv("DISCORD_LOG_WEBHOOK")
+
+class DiscordLogHandler(logging.Handler):
+    def emit(self, record):
+        log_entry = self.format(record)
+        if record.levelno >= logging.ERROR:
+            log_entry = f"<@squishvocado>\n**[ERROR]**\n{log_entry}"
+        else:
+            log_entry = f"[LOG] {log_entry}"
+        asyncio.create_task(self.send(log_entry))
+
+    async def send(self, message):
+        async with aiohttp.ClientSession() as session:
+            await session.post(WEBHOOK_URL, json={"content": message})
